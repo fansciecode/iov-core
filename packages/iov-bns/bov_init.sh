169ba0936f44e1d5bc016d1be667864f38960a5d
Alex Peters <alpe@users.noreply.github.com>
2018-10-25 16:35:48 +0200

    Use custom multicoin genesis file

--- packages/iov-bns/bov_init.sh
+++ packages/iov-bns/bov_init.sh
@@ -10,5 +10,54 @@ chmod 777 "${BOV_DIR}"
 docker run -v "${BOV_DIR}:/tendermint" \
   "iov1/tendermint:${TM_VERSION}" init
 
+# replace genesis
+rm -f "${BOV_DIR}/config/genesis.json"
+echo '
+{
+  "app_hash": "",
+  "app_state": {
+    "wallets": [
+      {
+        "address": "b1ca7e78f74423ae01da3b51e676934d9105f282",
+        "name": "admin",
+        "coins": [
+          {
+            "whole": 123456789,
+            "ticker": "CASH"
+          },
+          {
+            "whole": 123456789,
+            "ticker": "ALX"
+          }
+        ]
+      }
+    ],
+    "tokens": [
+      {
+        "ticker": "CASH",
+        "name": "Main token of this chain",
+        "sig_figs": 6
+      },
+      {
+        "ticker": "ALX",
+        "name": "Another token of this chain",
+        "sig_figs": 6
+      }
+    ]
+  },
+  "chain_id": "test-chain-32fU3N",
+  "genesis_time": "2018-10-25T13:58:22.4420972Z",
+  "validators": [
+    {
+      "pub_key": {
+        "type": "AC26791624DE60",
+        "value": "iIP6ld4L8D1xGGjelUbXiI7MuOaonTdYjnDRQVqwyPU="
+      },
+      "power": 10,
+      "name": ""
+    }
+  ]
+}
+' > "${BOV_DIR}/config/genesis.json"
 docker run -v "${BOV_DIR}:/data" "iov1/bnsd:${BOV_VERSION}" -home "/data" \
-  init CASH b1ca7e78f74423ae01da3b51e676934d9105f282
+  init -i
